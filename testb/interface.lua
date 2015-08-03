7b595ab159a9236dcef2ceff39372467531b1c2f
eligo <596840317@qq.com>
2015-08-04 00:06:25 +0800

    aa

--- testb/interface.lua
+++ testb/interface.lua
@@ -1,4 +1,3 @@
---这个文件主要实现供底层驱动上层的函数(c_onXxxx这种)
 package.cpath = string.format("%s;%s?.so", package.cpath, './3rd/luaso/')	--设置外部c库的搜索路径
 local env = require("lualib.class").singleton("env")
 local posed = require("lualib.class").singleton("posed")
